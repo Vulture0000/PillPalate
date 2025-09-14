8c77f1af820f37e26013a42bda89757f0bbd851f
Vulture0000 <sharkram4170@gmail.com>
2025-09-14 21:29:59 +0530

    duplicates has filled with 0

--- cleaning.py
+++ cleaning.py
@@ -1,8 +1,14 @@
 import numpy as np 
 import pandas as pd
+from sklearn.model_selection import test_train_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
 
-df = pd.read_csv('./Dataset/as7265x_synthetic_dataset.csv')
+df_not_cleaned = pd.read_csv('./Dataset/as7265x_synthetic_dataset.csv')
 
-print(df.head)
+# print(df.head)
 
-df_clean = df.fillna(0)
+df = df_not_cleaned.fillna(0) 
+
+# print(df.isnull().sum())
+
